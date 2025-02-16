cf3b5caf1eb2e6cf240a7dd0a3207460109e6e65
MrChaker <mr.chakerw@gmail.com>
2025-02-16 22:10:59 +0100

    Fix top bar style

--- src-tauri/src/scripts/app_bar.js
+++ src-tauri/src/scripts/app_bar.js
@@ -2,9 +2,9 @@ const closeBtnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="
 
 const hideBtnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minus"><path d="M5 12h14"/></svg>`;
 
-const minimizeBtnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="#000" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-2"><polyline points="4 14 10 14 10 20"/><polyline points="20 10 14 10 14 4"/><line x1="14" x2="21" y1="10" y2="3"/><line x1="3" x2="10" y1="21" y2="14"/></svg>`;
+const minimizeBtnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-2"><polyline points="4 14 10 14 10 20"/><polyline points="20 10 14 10 14 4"/><line x1="14" x2="21" y1="10" y2="3"/><line x1="3" x2="10" y1="21" y2="14"/></svg>`;
 
-const maximizeBtnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-2"><polyline points="4 14 10 14 10 20"/><polyline points="20 10 14 10 14 4"/><line x1="14" x2="21" y1="10" y2="3"/><line x1="3" x2="10" y1="21" y2="14"/></svg>`;
+const maximizeBtnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize-2"><polyline points="15 3 21 3 21 9"/><polyline points="9 21 3 21 3 15"/><line x1="21" x2="14" y1="3" y2="10"/><line x1="3" x2="10" y1="21" y2="14"/></svg>`;
 
 const settingsBtnSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-settings"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"/><circle cx="12" cy="12" r="3"/></svg>`;
 
@@ -81,6 +81,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   `;
 
+  // wrap the body content in a div
+  const body_content = document.createElement("div");
+  body_content.innerHTML = document.body.innerHTML;
+  body_content.style.overflowY = "auto";
+  body_content.style.height = "100%";
+
+  document.body.innerHTML = body_content.outerHTML;
+  document.body.style.overflow = "hidden";
+  document.body.style.height = "100vh";
+
   const app_bar = document.createElement("div");
   app_bar.id = "appbar";
   app_bar.innerHTML = app_bar_html;
@@ -114,22 +124,22 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("close-btn").addEventListener("click", () => {
     appWindow.close();
   });
-});
 
-document.addEventListener("scroll", () => {
-  const elements = document.querySelectorAll("*");
-  elements.forEach((element) => {
-    if (element.id === "appbar") {
-      return;
-    }
-    const computedStyle = window.getComputedStyle(element);
-    const position = computedStyle.position;
-
-    if (position === "fixed" || position === "sticky") {
-      const currentTop = computedStyle.top;
-      if (currentTop === "0px") {
-        element.style.top = `${barHeight}px`;
+  document.addEventListener("scroll", () => {
+    const elements = document.querySelectorAll("*");
+    elements.forEach((element) => {
+      if (element.id === "appbar") {
+        return;
       }
-    }
+      const computedStyle = window.getComputedStyle(element);
+      const position = computedStyle.position;
+
+      if (position === "fixed" || position === "sticky") {
+        const currentTop = computedStyle.top;
+        if (currentTop === "0px") {
+          element.style.top = `${barHeight}px`;
+        }
+      }
+    });
   });
 });
