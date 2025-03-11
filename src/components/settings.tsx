e1b42277a4c12ed602f94ec7f2968017958062fe
MrChaker <mr.chakerw@gmail.com>
2025-03-11 14:13:33 +0100

    Change Interval to seconds

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -74,7 +74,7 @@ const LicenseInfo = ({ db }: { db: Database | null }) => {
   const heartbeat = () => {
     const clearInterval = setInterval(() => {
       pingHeartbeat((license as any).id, license?.key!);
-    }, import.meta.env.VITE_HEARTBEAT_INTERVAL * 60 * 1000 - 20000); // ping before 20 seconds of end
+    }, import.meta.env.VITE_HEARTBEAT_INTERVAL * 1000 - 10000); // ping before 10 seconds of end
     return clearInterval;
   };
 
