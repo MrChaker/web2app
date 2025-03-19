30f5b218b2c049d8c0f36f3afd09bce38a78ad95
MrChaker <mr.chakerw@gmail.com>
2025-03-19 19:06:31 +0100

    Fix build

--- install.cjs
+++ install.cjs
@@ -5,14 +5,20 @@ const { platform } = require("os");
 try {
   switch (platform()) {
     case "linux":
-      execSync("npm install @tauri-apps/cli-linux-x64-gnu --no-save", {
-        stdio: "inherit",
-      });
+      execSync(
+        "npm install @rollup/rollup-linux-x64-gnu @tauri-apps/cli-linux-x64-gnu --no-save",
+        {
+          stdio: "inherit",
+        }
+      );
       break;
     case "win32":
-      execSync("npm install @tauri-apps/cli-win32-x64-msvc --no-save", {
-        stdio: "inherit",
-      });
+      execSync(
+        "npm install @rollup/rollup-win32-x64-msvc @tauri-apps/cli-win32-x64-msvc  --no-save",
+        {
+          stdio: "inherit",
+        }
+      );
       break;
     default:
       console.log("No OS-specific package for this platform.");
